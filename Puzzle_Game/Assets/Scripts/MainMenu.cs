0bbad9b385f5ed103470217b1d25e7399c117071
Bastion616 <albert.sudmann@outlook.com>
2022-11-02 15:48:25 +0100

    Transitions

--- Puzzle_Game/Assets/Scripts/MainMenu.cs
+++ Puzzle_Game/Assets/Scripts/MainMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    
   
 public void PlayGame() {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1) ;
@@ -15,3 +16,40 @@ public void QuitGame() {
 }
 
 }
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+  
+public void PlayGame() {
+
+    StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    IEnumerator LoadLevel(int levelIndex) {
+    transition.SetTrigger("Start");
+
+    yield return new WaitForSeconds(transitionTime);
+
+    SceneManager.LoadScene(levelIndex);
+}
+}
+
+
+
+public void QuitGame() {
+    Debug.Log("Quit Game!");    
+    Application.Quit();
+}
+
+}
+
+
+
+*/
